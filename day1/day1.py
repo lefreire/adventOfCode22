74a1cbec7d6be41c350de9dde2569642e4e5e4a3
lefreire <letfreirefigueiredo@gmail.com>
2022-12-06 21:02:14 -0300

    day one part 2

--- day1/day1.py
+++ day1/day1.py
@@ -16,5 +16,27 @@ def verify_input():
 	print(best_elf)
 
 
+def get_three_elf():
+	input = read_file()
+	best_elf = [0, 0, 0]
+	elf = 0
+	for i in input:
+		if i == '\n':
+			for i in best_elf:
+				if elf > i: 
+					best_elf[best_elf.index(min(best_elf))] = elf
+					break
+			elf = 0
+		else:
+			elf += int(i)
+	for i in best_elf:
+		if elf > i: 
+			best_elf[best_elf.index(min(best_elf))] = elf
+			break
+	print(sum(best_elf))
+
+
+
 if __name__ == '__main__':
-	verify_input()
+	# verify_input()
+	get_three_elf()
